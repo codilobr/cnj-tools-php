9276c05ce23466388566d5dd1643cb74cc6acc91
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-06-11 18:09:14 -0400

    Implementação da função de genValidator

--- src/CnjTools.php
+++ src/CnjTools.php
@@ -9,7 +9,8 @@ class CnjTools
       return $a + $b;
   }
 
-  public function checkValidator($NNNNNNN, $DD, $AAAA, $J, $TR, $OOOO) {
+  public function checkValidator($NNNNNNN, $DD, $AAAA, $J, $TR, $OOOO)
+  {
     $nnnnnnn = str_pad($NNNNNNN, 7, '0', STR_PAD_LEFT);
     $dd = str_pad($DD, 2, '0', STR_PAD_LEFT);
     $aaaa = str_pad($AAAA, 4, '0', STR_PAD_LEFT);
@@ -23,5 +24,26 @@ class CnjTools
     $value2 = $mod2 . $oooo . $dd;
 
     return ($value2 % 97) == 1;
+  }
+
+  public function genValidator ($NNNNNNN, $AAAA, $J, $TR, $OOOO)
+  {
+    $nnnnnnn = str_pad($NNNNNNN, 7, '0', STR_PAD_LEFT);
+    $aaaa = str_pad($AAAA, 4, '0', STR_PAD_LEFT);
+    $j = substr($J, 0, 1);
+    $tr = str_pad($TR, 2, '0', STR_PAD_LEFT);
+    $oooo = str_pad($OOOO, 4, '0', STR_PAD_LEFT);
+
+    $block1 = $nnnnnnn . $aaaa;
+    $mod1 = ((int) $block1) % 97;
+    $value1 = $mod1 * 1000000000;
+    $block2 = $j . $tr . $oooo;
+    $value2 = ((int) $block2) * 100;
+    $mod2 = ($value1 + $value2) % 97;
+
+    $dd = str_pad(98 - $mod2, 2, '0', STR_PAD_LEFT);
+    $cnj = $nnnnnnn . '-' . $dd . '.' . $aaaa . '.' . $j . '.' . $tr . '.' . $oooo;
+
+    return $cnj;
   }
 }
